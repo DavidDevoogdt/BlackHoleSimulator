b0e1e3e62acfa76173605b881f781b2a47e90c58
David Devoogdt <David.devoogdt@gmail.com>
2020-04-05 22:10:43 +0200

    extended plotting capabilities

--- src/plotter.py
+++ src/plotter.py
@@ -2,34 +2,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
-
+from mpl_toolkits.mplot3d import Axes3D
 
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-N', '--number' , type=int, nargs='+',
-                    help='number of csv files')
+parser.add_argument('-N', '--number' , type=int, nargs=1, help='number of csv files')
+parser.add_argument('-M', '--mode', nargs=1, help='mode: example xy or xyz')
 
 
 args = parser.parse_args()
 
+print(args)
 
 N = int( args.number[0])
+name = args.mode[0][1:]
 
 fig = plt.figure()
 
+if name == 'xz': 
+    print("ddddddddddddd")
+    plt.axis('equal')
+    plt.xlim(-5 ,5)
+    plt.ylim(-5,10)
 
-circle1 = plt.Circle((0, 0), 1, color='black')
-plt.gcf().gca().add_artist(circle1)
-plt.axis('equal')
-plt.xlim(-5 ,5)
-plt.ylim(-5,10)
+    for i in range(0,N):
+        db = pd.read_csv( 'files/photon%d.csv'%i, names=['t','x','y','z','pt','px','py','pz'])  
+        #print(db)
+        plt.plot( db['x'],db['z']  )
+if name == 'xy': 
+    plt.axis('equal')
+    plt.xlim(-5 ,5)
+    plt.ylim(-5,10)
 
-for i in range(0,N):
-    #db = pd.read_csv( 'files/photon%d.csv'%i, names=['t','r','p','h','pt','pr','pp','ph'])  
-    db = pd.read_csv( 'files/photon%d.csv'%i, names=['t','x','y','z','pt','px','py','pz'])   
-    print(db)
-    plt.plot( db['x'],db['z']  )
-    #plt.plot( db['r']*np.sin(db['h'])*np.cos(db['p']),db['r']*np.cos(db['h']))
+    for i in range(0,N):
+        db = pd.read_csv( 'files/photon%d.csv'%i, names=['t','x','y','z','pt','px','py','pz'])  
+        #print(db)
+        plt.plot( db['x'],db['y']  )
+elif name == 'xyz':
+    ax = fig.add_subplot(111, projection='3d')
+
+    #plt.axis('equal')
+    plt.xlim(-5 ,5)
+    plt.ylim(-5,5)
+    ax.set_zlim(-5,5)
+
+    for i in range(0,N):
+        db = pd.read_csv( 'files/photon%d.csv'%i, names=['t','x','y','z','pt','px','py','pz'])  
+        #print(db)
+        ax.plot( db['x'], db['y'],db['z'])
 
 
 plt.show()
--- src/python_interface.rs
+++ src/python_interface.rs
@@ -3,22 +3,20 @@ extern crate csv;
 use std::error::Error;
 use std::process::Command;
 
-pub fn launch_python(n :i32){
+pub fn launch_python(n :i32, mode : &str){
     Command::new("python3")
             .arg("src/plotter.py")
             .arg(format!("-N {}",n) )
+            .arg(format!("-M {}",mode) )
             .spawn()
             .expect("error while calling python");
-
 }
 
-pub fn save_to_csv(v : &Vec< [f64;8] >, name : String) -> Result<(), Box<dyn Error>> {
+pub fn save_to_csv<S: serde::Serialize>(v : &Vec< S >, name : String) -> Result<(), Box<dyn Error>> {
     let mut wtr = csv::Writer::from_path(  name )?;
-
     for x in v {
         wtr.serialize( x )?;
     }
-
     wtr.flush()?;
     Ok(())
 }
